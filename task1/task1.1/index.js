fec36ae62ab384a88ca001b4808658688421abc2
Ilya Kolmakov <ilkolmakov@yandex.ru>
2021-10-05 01:31:46 +0500

    1 fix task 1.1

--- task1/task1.1/index.js
+++ task1/task1.1/index.js
@@ -1,10 +1,10 @@
+const del = require('del');
 const fs = require('fs');
 const path = require('path');
 
 const startDir = process.argv[2];
 const resultDir = process.argv[3];
-const level = +process.argv[4] || 3;
-const isDeletedStartDir = process.argv[5] === 'delete' ? true : false;
+const deleteFlag = process.argv[4] === 'true' ? true : false;
 
 if (!startDir) {
     console.log('Specify the start directory');
@@ -16,93 +16,85 @@ if (!resultDir) {
     return;
 }
 
-const readDir = (base, currentLevel) => {
-    if (currentLevel <= 0) {
-      return;
+const transformFiles = (base, result, deleteFlag) => {
+  fs.access(base, (err) => {
+    if (err) {
+      throw new Error('Ошибка проверки исходной папки');
     }
-    const files = fs.readdirSync(base);
-    files.forEach((item) => {
-      let localBase = path.join(base, item);
-      let state = fs.statSync(localBase);
-      if (state.isDirectory()) {
-        readDir(localBase, currentLevel - 1);
-      } else {
-        if ( !fs.existsSync(resultDir) ) {
-          fs.mkdirSync(resultDir)
-        }
 
-        const innerDir =  item[0].toUpperCase();
 
-        if ( !fs.existsSync(path.join(resultDir, innerDir)) ) {
-          fs.mkdirSync(path.join(resultDir, innerDir))
-        }
-
-        if ( !fs.existsSync(path.join(resultDir, innerDir, item)) ) {
-            fs.linkSync(path.join(base, item), path.join(resultDir, innerDir, item));
-        }
-
-        if (isDeletedStartDir) {
-            fs.unlinkSync(path.join(base, item));
-
-            if (fs.readdirSync(base).length === 0) {
-                fs.rmdirSync(base);
-            }
-
-            if (fs.readdirSync(startDir).length === 0) {
-                fs.rmdirSync(startDir);
-            }
-        }
+    fs.access(result, (err) => {
+      if (err) {
+        fs.mkdir(result, (err) => {
+          if (err) {
+            throw new Error('Ошибка создания результирующей папки');
+          }
+        });
       }
     });
+
+    fs.readdir(base, (err, files) => {
+
+      if (err) {
+        throw new Error('Ошибка чтения директории');
+      }
+
+      files.forEach((item) => {
+        let localBase = path.join(base, item);
+        fs.stat(localBase, (err, state) => {
+          if (err) {
+            throw new Error('Ошибка проверки файла');
+          }
+
+          if (state.isDirectory()) {
+            transformFiles(localBase, result, deleteFlag);
+          } else {
+            const resultInnerDir =  item[0].toUpperCase();
+
+            fs.access(path.join(resultDir, resultInnerDir), (err) => {
+              if (err) {
+                fs.mkdir(path.join(resultDir, resultInnerDir), (err) => {
+                  if (err) {}
+                })
+              }
+
+              if (deleteFlag) {
+                fs.rename(path.join(base, item), path.join(resultDir, resultInnerDir, item), (err) => {
+                  if (err) {
+                    fs.copyFile(path.join(base, item), path.join(resultDir, resultInnerDir, item), (err) => {
+                      if (err) {
+                        throw new Error('Ошибка копирования файла');
+                      }
+
+                      fs.unlink(path.join(base, item), (err) => {
+                        if (err) {
+                          throw new Error('Ошибка удаления файла');
+                        }
+                      });
+                    });
+                  }
+                });
+              } else {
+                fs.copyFile(path.join(base, item), path.join(resultDir, resultInnerDir, item), (err) => {
+                  if (err) {
+                    throw new Error('Ошибка копирования файла');
+                  }
+                });
+              }
+            });
+          }
+        })
+      });
+    });
+  });
 }
 
-readDir(startDir, level);
+try {
+  transformFiles(startDir, resultDir, deleteFlag);
+  if (deleteFlag) {
+    del(startDir);
+  }
+} catch (err) {
+  console.error(err);
+}
 
-// const path = require('path');
-// const util = require('util');
-// const fs = require('fs');
-
-// const readdir = util.promisify(fs.readdir);
-
-// const config = {
-//     typeDirs: [
-//         { type: '.pdf', directory: 'documents' },
-//         { type: '.png', directory: 'images' },
-//         { type: '.mp3', directory: 'music' },
-//     ]
-// }
-
-// const directory = process.argv[2];
-
-// if (!directory) {
-//     console.log('Specify the target directory');
-//     return;
-// }
-
-// [...config.typeDirs, { directory: 'other' }].map((dir) => {
-//   const dirname = `${directory}/${d.directory}`;
-//   if ( !fs.exists(directory) ) {
-//     fs.mkdirSync(dirname)
-//   }
-// });
-
-// (async () => {
-//   const files = await readdir(directory);
-//   files.forEach((file) => {
-//     const extname = path.extname(file);
-//     if (!extname) {
-//       return;
-//     }
-
-//     const { directory: targetDir = 'other' } = config.typeDirs.find((dir) => dir.type === extname) || {};
-
-//     const oldPath = path.join(__dirname, directory, file);
-//     const newPath = path.join(__dirname, directory, targetDir, file);
-
-//     fs.rename(oldPath, newPath, (err) => {
-//       if (err) {
-//         throw err;
-//       }
-//     });
-//   });
-// })();
